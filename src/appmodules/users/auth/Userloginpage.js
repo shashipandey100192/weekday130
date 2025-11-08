f266b6aa89ad51fc3b6aad16f8e35e56997fdc3f
sjdfhy <shasij>
2025-11-08 14:44:01 +0530

    form validation

--- src/appmodules/users/auth/Userloginpage.js
+++ src/appmodules/users/auth/Userloginpage.js
@@ -3,10 +3,20 @@ import { MdOutlineMailOutline } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { FaUser } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form"
 
 
 function Userloginpage() {
+
+const {register,handleSubmit,formState: { errors }} = useForm();
+
+const myform = (d)=>{
+    console.log(d);
+}
+
+
   return (
+    <form onSubmit={handleSubmit(myform)}>
     <div className='container'>
             <div className='row justify-content-center'>
                 <div className='col-md-5 bg-light shadow p-5'>
@@ -22,19 +32,21 @@ function Userloginpage() {
                             <div className='col-md-12'>
                                 <div className="mb-3">
                                     <label className="form-label"> <MdOutlineMailOutline/> Email address</label>
-                                    <input type="email" className="form-control"/>
+                                    <input type="email" className="form-control" {...register("email",{required:true})}/>
+                                    {errors.email && <p className='text-danger'> email is required</p>}
                                 </div>
                             </div>
                              <div className='col-md-12'>
                                 <div className="mb-3">
                                     <label className="form-label"> <RiLockPasswordLine/> Password</label>
-                                    <input type="password" className="form-control" />
+                                    <input type="password" className="form-control" {...register("pass",{required:true})}/>
+                                     {errors.pass && <p className='text-danger'> password is required</p>}
                                 </div>
                             </div>
                              <div className='col-md-12 text-center'>
                                 <div className="mb-3">
                                    
-                                    <input type='button' value="login" className='btn btn-success'/>
+                                    <input type='submit' value="login" className='btn btn-success'/>
                                     <Link to="registor" className='ms-3'>new registor</Link>
                                 </div>
                             </div>
@@ -43,6 +55,7 @@ function Userloginpage() {
                 </div>
             </div>
         </div>
+        </form>
   )
 }
 
